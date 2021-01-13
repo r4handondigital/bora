a97c608a5ed8a7fed3687931114fb0ac2a125f81
R4HANDON <44488041+rondinelle03@users.noreply.github.com>
2021-01-12 21:55:01 -0300

    inicio da mintage0
    n6v5

---
+++ libs/geral.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+
+
+
+/*var nav = $('.lindddhas-mobile');
+    
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            nav.addClass("f-nav");
+        } else {
+            nav.removeClass("f-nav");
+        }
+    });*/
+
+});
